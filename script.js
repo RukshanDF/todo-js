3aef7c0a4581672f5c513cbc927d92119f0db8d9
Rukshan <rukshan.dhamushka@gmail.com>
2020-07-04 22:06:11 +0530

    finished items

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var i = 0;
 
 function add(){
-    document.getElementById('element').innerHTML += '<li id="todo-item-' + i + '"><input type="checkbox" /><span class="todo1"></span><input type="button" value="x" class="close" onclick="deletEntry(' + i + ')" /></li>' 
+    document.getElementById('element').innerHTML += '<li id="todo-item-' + i + '"><input type="checkbox" onchange="cancelItem(event, '+ i +')" /><span class="todo1"></span><input type="button" value="x" class="close" onclick="deletEntry(' + i + ')" /></li>' 
     document.getElementsByClassName('todo1')[i].innerHTML = document.getElementsByClassName('new')[0].value;
     i++;
 } 
@@ -9,4 +9,21 @@ function add(){
 function deletEntry(index){
     document.getElementById('todo-item-' + index ).remove();
     i--;  
+}
+
+function cancelItem(e, index){
+    var original;
+    var newElement;
+
+    if (e.target.checked) {
+        original = document.querySelector('#todo-item-' + index + ' span');
+        newElement = document.createElement('del');
+        newElement.innerHTML = original.innerHTML;
+        original.replaceWith(newElement);
+    } else {
+        original = document.querySelector('#todo-item-' + index + ' del'); 
+        newElement = document.createElement('span');
+        newElement.innerHTML = original.innerHTML;
+        original.replaceWith(newElement);
+    }
 }
