433d0df9c05906a7251ddcaf28152446a943952e
Rukshan <rukshan.dhamushka@gmail.com>
2020-07-04 20:15:39 +0530

    add entery function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert('hello');
+var i = 0;
+
+function add(){
+    document.getElementById('element').innerHTML += '<li id="todo-item-' + i + '"><input type="checkbox" /><span class="todo1"></span><input type="button" value="x" class="close" onclick="deletEntry(' + i + ')" /></li>' 
+    document.getElementsByClassName('todo1')[i].innerHTML = document.getElementsByClassName('new')[0].value;
+    i++;
+} 
+
